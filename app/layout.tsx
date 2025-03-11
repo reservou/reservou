6c419a530744199b44c39ddbbf619d1382b0b91e
pedrodevelops <dev.henriquegn@gmail.com>
2025-03-10 22:02:15 -0300

    chore: init nextjs application

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Montserrat } from 'next/font/google';
+import "./globals.css";
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '700'], 
+  variable: '--font-montserrat',
+});
+
+export const metadata: Metadata = {
+  title: "Rezervou",
+  description: "Reservation platform for the modern age",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className + 'dark:bg-amber-950'}>{children}</body>
+    </html>
+  );
+}
