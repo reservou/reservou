e18be9b3750bbcf7ea7ecd2345b6db692933e371
pedrodevelops <dev.henriquegn@gmail.com>
2025-03-11 16:32:15 -0300

    chore: shadcn components setup
    chore: biome formatter and linter setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,29 @@
+import { cn } from "@/src/lib/utils";
 import type { Metadata } from "next";
-import { Montserrat } from 'next/font/google';
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['400', '700'], 
-  variable: '--font-montserrat',
+	subsets: ["latin"],
+	weight: ["400", "700"],
+	variable: "--font-montserrat",
 });
 
 export const metadata: Metadata = {
-  title: "Rezervou",
-  description: "Reservation platform for the modern age",
+	title: "Rezervou",
+	description: "Reservation platform for the modern age",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={montserrat.className + 'dark:bg-amber-950'}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={cn(montserrat.className, "bg-amber-50")}>
+				{children}
+			</body>
+		</html>
+	);
 }
