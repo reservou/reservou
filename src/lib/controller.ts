9ad24c51408c5957ed31e66cf49ac80bc0640d0a
pedrodevelops <dev.henriquegn@gmail.com>
2025-03-14 17:36:53 -0300

    refactor: improve controllers response format for api route handlers

--- src/lib/controller.ts
+++ src/lib/controller.ts
@@ -1,45 +1,105 @@
+import { type ErrorObject, HttpError, InternalServerError } from "@/src/errors";
 import { NextResponse } from "next/server";
 
-import { HttpError, InternalServerError } from "@/src/errors";
+type BaseResponse<Data = unknown> = {
+	data: Data | null;
+	error: ErrorObject | null;
+	message: string;
+};
 
 type ControllerParams = Parameters<(...args: never[]) => Promise<NextResponse>>;
 
-type Controller<T = unknown> = (
+type Controller<Body extends BaseResponse> = (
 	...params: ControllerParams
-) => Promise<NextResponse<T>>;
+) => Promise<NextResponse<Body>>;
 
-type WrappedController<T = unknown> = (
-	...params: ControllerParams
-) => Promise<NextResponse<T>>;
-
-export function buildController<T = unknown>(
-	controller: Controller<T>,
-): WrappedController<T> {
-	return async (...params: ControllerParams): Promise<NextResponse<T>> => {
+/**
+ * Enforces a uniform { data, error, message } response structure for API consistency,
+ * while concealing sensitive error details from clients for security.
+ */
+function buildController<Data = unknown>(
+	controller: Controller<BaseResponse<Data>>,
+) {
+	return async (
+		...params: ControllerParams
+	): Promise<NextResponse<BaseResponse<Data>>> => {
 		try {
 			return await controller(...params);
-		} catch (error) {
-			if (
-				error instanceof InternalServerError ||
-				!(error instanceof HttpError)
-			) {
-				/**
-				 * @todo Add logger for critical error
-				 */
-				console.error(error);
+		} catch (caughtError) {
+			const userSafeError =
+				caughtError instanceof HttpError &&
+				!(caughtError instanceof InternalServerError);
+			const supportCriticalError = caughtError instanceof InternalServerError;
+			const unknownCriticalError = !(caughtError instanceof HttpError);
 
+			if (userSafeError) {
+				const httpError = caughtError as HttpError;
 				return NextResponse.json(
 					{
+						data: null,
+						error: {
+							code: httpError.code,
+							correlationId: httpError.correlationId,
+							details: httpError.details ?? httpError.message,
+							message: httpError.message,
+							status: httpError.status,
+							statusCode: httpError.statusCode,
+						},
+						message: httpError.message,
+					},
+					{
+						status: httpError.statusCode,
+						statusText: httpError.status,
+					},
+				);
+			}
+
+			/**
+			 * @todo Replace console.error when logger is implement to log critical errors
+			 */
+			if (supportCriticalError) {
+				const internalError = caughtError as InternalServerError;
+				console.error(internalError);
+				return NextResponse.json(
+					{
+						data: null,
+						error: {
+							code: internalError.code,
+							correlationId: internalError.correlationId,
+							details: internalError.details ?? internalError.message,
+							message: internalError.message,
+							status: internalError.status,
+							statusCode: internalError.statusCode,
+						},
 						message: "Um erro inesperado aconteceu, o suporte foi notificado.",
 					},
 					{ status: 500 },
-				) as NextResponse<T>;
+				);
+			}
+
+			if (unknownCriticalError) {
+				console.error(caughtError);
+				return NextResponse.json(
+					{
+						data: null,
+						error: null,
+						message: "Um erro inesperado aconteceu, o suporte foi notificado.",
+					},
+					{ status: 500 },
+				);
 			}
 
 			return NextResponse.json(
-				{ message: error.message },
-				{ status: error.statusCode, statusText: error.status },
-			) as NextResponse<T>;
+				{
+					data: null,
+					error: null,
+					message: "Erro desconhecido.",
+				},
+				{ status: 500 },
+			);
 		}
 	};
 }
+
+export { buildController };
+export type { BaseResponse };
