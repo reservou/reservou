f218e678a885b4ebbb9be911efb02e2cb5baa435
pedrodevelops <dev.henriquegn@gmail.com>
2025-03-26 16:15:37 -0300

    chore: adjust next config remote patterns for google storage, adjust body size limit to 6mb

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	experimental: {
+		serverActions: {
+			bodySizeLimit: "6mb",
+		},
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "storage.googleapis.com",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
