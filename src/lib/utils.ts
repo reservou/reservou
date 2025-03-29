7e02e5831a816de01a88550e6871c41daf56fce6
pedrodevelops <dev.henriquegn@gmail.com>
2025-03-28 21:48:23 -0300

    feat: add image compression util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
+import imageCompression from "browser-image-compression";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
+}
+
+export function sleep(ms: number) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export async function compressImage(file: File): Promise<File | null> {
+	try {
+		const options = {
+			maxSizeMB: 1, // Max size of 1MB per photo
+			maxWidthOrHeight: 1920, // Resize to max 1920px width or height
+			useWebWorker: true, // Offload to web worker for performance
+		};
+		const compressedFile = await imageCompression(file, options);
+		return compressedFile;
+	} catch (error) {
+		return null;
+	}
 }
