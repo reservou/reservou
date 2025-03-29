7c65faecceef174fa46de772c021f2f8ff17fae8
pedrodevelops <dev.henriquegn@gmail.com>
2025-03-28 21:23:53 -0300

    chore: add cloudinary domain to next config

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,12 @@ const nextConfig: NextConfig = {
 				port: "",
 				pathname: "/**",
 			},
+			{
+				protocol: "https",
+				hostname: "res.cloudinary.com",
+				port: "",
+				pathname: "/**",
+			},
 		],
 	},
 };
